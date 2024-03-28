481652d1e4213b30af1ecb22ebeddde21d481e5e
venkatesh <kasivenkatesh12@gmail.com>
2024-03-28 14:36:30 +0530

    customuser

--- customuser/customuser/settings.py
+++ customuser/customuser/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'userapp'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -115,7 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static/',
+AUTH_USER_MODEL='userapp.NewUser',
+AUTH_GROUPS_MODEL='userapp.Dealer',
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- customuser/userapp/admin.py
+++ customuser/userapp/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.forms import Textarea
+from .models import NewUser
+from .models import Dealer
 
-# Register your models here.
+class UserAdminConfig(UserAdmin):
+    model = NewUser
+    search_fields = ('email', 'user_name', 'first_name',)
+    list_filter = ('email', 'user_name', 'first_name', 'is_active', 'is_staff')
+    ordering = ('-start_date',)
+    list_display = ('email', 'user_name', 'first_name', 'is_active', 'is_staff')
+    fieldsets = (
+        (None, {'fields': ('email', 'user_name', 'first_name',)}),
+        ('Permissions', {'fields': ('is_staff', 'is_active',)}),
+        ('Personal', {'fields': ('about',)})
+    )
+
+    # Override form field for 'about' field
+    formfield_overrides = {
+        NewUser.about: {'widget': Textarea(attrs={'rows': 10, 'cols': 40})}
+    }
+
+    # Customized add fields for User creation
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'user_name', 'first_name', 'password1', 'password2', 'is_active', 'is_staff',)
+        }),
+    )
+
+# Register the NewUser model with the customized UserAdminConfig
+admin.site.register(NewUser, UserAdminConfig)
+admin.site.register(Dealer,UserAdminConfig)
