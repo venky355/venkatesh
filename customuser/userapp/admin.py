f672afa36fb88783a90a8d58d48941183c9acdb5
venkatesh <kasivenkatesh12@gmail.com>
2024-03-28 15:51:41 +0530

    customuser

--- customuser/userapp/admin.py
+++ customuser/userapp/admin.py
@@ -1,42 +1,8 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from django.forms import Textarea
 from .models import NewUser
 from .models import Dealer
 
-class UserAdminConfig(UserAdmin):
-    model = NewUser
-    search_fields = ('email', 'user_name', 'first_name',)
-    list_filter = ('email', 'user_name', 'first_name', 'is_active', 'is_staff')
-    ordering = ('-start_date',)
-    list_display = ('email', 'user_name', 'first_name', 'is_active', 'is_staff')
-    fieldsets = (
-        (None, {'fields': ('email', 'user_name', 'first_name',)}),
-        ('Permissions', {'fields': ('is_staff', 'is_active',)}),
-        ('Personal', {'fields': ('about',)})
-    )
 
-    
-    formfield_overrides = {
-        NewUser.about: {'widget': Textarea(attrs={'rows': 10, 'cols': 40})}
-    }
 
-    
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'user_name', 'first_name', 'password1', 'password2', 'is_active', 'is_staff',)
-        }),
-    )
-
-admin.site.register(NewUser, UserAdminConfig)
-
-class DealerAdmin(admin.ModelAdmin):
-    
-    model = Dealer
-    search_fields = ('name')
-    list_filter = ('name','location','group')
-    
-    list_display = ('name','location','group')
-
-admin.site.register(Dealer, DealerAdmin)
+admin.site.register(NewUser)
+admin.site.register(Dealer)
--- customuser/userapp/models.py
+++ customuser/userapp/models.py
@@ -1,63 +1,18 @@
 from django.db import models
-from django.utils import timezone
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager, Permission, Group
+from django.contrib.auth.models import AbstractUser,Permission, Group
 from django.utils.translation import gettext_lazy as _
 
-class CustomAccountManager(BaseUserManager):
-    """
-    A custom manager for handling user creation.
-    """
-
-    def create_superuser(self, email, user_name, first_name, password, **other_fields):
-        """
-        Creates and saves a superuser with the given email, username, first name, and password.
-        """
-        other_fields.setdefault('is_staff', True)
-        other_fields.setdefault('is_superuser', True)
-        other_fields.setdefault('is_active', True)
-
-        if other_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-
-        return self.create_user(email, user_name, first_name, password, **other_fields)
-
-    def create_user(self, email, user_name, first_name, password, **other_fields):
-        """
-        Creates and saves a user with the given email, username, first name, and password.
-        """
-        if not email:
-            raise ValueError(_('You must provide an email address.'))
-
-        email = self.normalize_email(email)
-        user = self.model(email=email, user_name=user_name, first_name=first_name, **other_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-class NewUser(AbstractBaseUser, PermissionsMixin):
-    """
-    A custom user model with email-based authentication.
-    """
+class NewUser(AbstractUser):
     email = models.EmailField(_('email address'), unique=True)
-    user_name = models.CharField(max_length=150, unique=True)
-    first_name = models.CharField(max_length=150, blank=True)
-    start_date = models.DateTimeField(default=timezone.now)
     about = models.TextField(_('about'), max_length=500, blank=True)
-    is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=False)
-
-    objects = CustomAccountManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['user_name']
+    REQUIRED_FIELDS = ['username']
 
     def __str__(self):
-        return self.user_name
+        return self.username
 
 class Dealer(models.Model):
-    """
-    A model representing a dealer.
-    """
     name = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
     groups = models.ManyToManyField(Group, related_name='dealers')
